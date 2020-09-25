0b7902408c2a8cac5282920f33f30eb19686adac
Bruno Rodrigues <brunorodrigues-78@hotmail.com>
2020-09-24 22:11:31 -0300

    commit

--- ted/src/view/Teste.java
+++ ted/src/view/Teste.java
@@ -1,5 +1,7 @@
 package view;
 
+import javax.swing.JOptionPane;
+
 import modelo.Retangulo;
 
 
@@ -7,13 +9,17 @@ public class Teste {
 
 	public static void main(String[] args) {
 		
+		String r1_string = JOptionPane.showInputDialog("Base: ");
+		String r2_string = JOptionPane.showInputDialog("Altura: ");
+		float r1 = Float.parseFloat(r1_string);
+		float r2 = Float.parseFloat(r2_string);
+		
+		Retangulo ret = new Retangulo(r1, r2);
 		
 		
-		Retangulo ret = new Retangulo(2, 2);
+		JOptionPane.showMessageDialog(null, "A área é de: "+ ret.calcularArea());
 		
 		
-		
-		System.out.println(ret.calcularArea());
 
 		
 	}
